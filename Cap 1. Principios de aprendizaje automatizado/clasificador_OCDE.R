a08f882bd2243dc27d4a8d6c4349fb221b5a5114
alfredo203 <alfredini31@hotmail.com>
2017-01-06 21:13:34 -0600

    clasificadores

--- Cap 1. Principios de aprendizaje automatizado/clasificador_OCDE.R
+++ Cap 1. Principios de aprendizaje automatizado/clasificador_OCDE.R
@@ -1,6 +1,8 @@
 ### clasificador de países OCDE
 
-## Lectura del archivo
+## Entrenamiento
+
+## Lectura del archivo de entrenamiento
 df <- read.csv(file = "Cap 1. Principios de aprendizaje automatizado/data_countries.csv")
 
 ## Se observan las variables
@@ -15,15 +17,28 @@ names(df) <- c("Nombre_pais",
                "GNI_PC_PPP",
                "OCDE_member")
 
+## Gráfica de dispersión de los datos seleccionados, sin distinción por clase
+par(mfrow=c(1,2))
 
-## Gráfica de dispersión de los datos seleccionados
 plot(df$Tasa_mortalidad, 
-     df$GNI_PC_PPP, col = df$OCDE)
+     df$GNI_PC_PPP, 
+     xlab = "Tasa de Mortalidad infantil por cada mil nacimientos (2015)",
+     ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
+
 
 ## Estimación del modelo de regresión lineal.
 ## se especifica que el ingreso nacional bruto = f(Tasa de mortalidad)
 
 a <- lm(data = df,  GNI_PC_PPP ~ Tasa_mortalidad)
+
+## Se pintan los casos que están mal clasificados
+df$OCDE_member[df$Tasa_mortalidad > 75] <- 3
+
+plot(df$Tasa_mortalidad, 
+     df$GNI_PC_PPP, col = df$OCDE,
+     xlab = "Tasa de Mortalidad infantil por cada mil nacimientos (2015)",
+     ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
+
 abline(a, col = "blue", lwd = 3)
 
 
@@ -35,27 +50,28 @@ library(dplyr)
 A <- read.csv("Cap 1. Principios de aprendizaje automatizado/all_contries.csv", stringsAsFactors = F)
 B<- read.csv("Cap 1. Principios de aprendizaje automatizado/ocde_members.csv", stringsAsFactors = F) %>% select(-Date)
 
-df <- left_join(A, B)
+df2 <- left_join(A, B)
 
-df$OCDE_member[is.na(df$OCDE_member)] <- 1
+df2$OCDE_member[is.na(df2$OCDE_member)] <- 1
 
-names(df) <-c("Nombre_pais",
+names(df2) <-c("Nombre_pais",
                "Codigo_pais",
                "GNI_PC_PPP",
                "Tasa_mortalidad",
                "OCDE_member")
+df2$OCDE_member[df2$Nombre_pais == "World"] <- 3
 
-plot(df$Tasa_mortalidad, 
-     df$GNI_PC_PPP, 
-     col = df$OCDE_member,
+
+par(mfrow=c(1,1))
+plot(df2$Tasa_mortalidad, 
+     df2$GNI_PC_PPP, 
+     col = df2$OCDE_member,
      xlab = "Tasa de Mortalidad infantil por cada mil nacimientos (2015)",
      ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
+abline(a, col = "blue", lwd = 3)
 
-b <- lm(data = df2,  GNI_PC_PPP ~ Tasa_mortalidad)
-abline(b, col = "red", lwd = 3)
+a$coefficients <- a$coefficients * -2
+abline(a, col = "blue", lwd = 3 )
 
-df3<-anti_join(df2, df)
-
-c <- lm(data = df3,  GNI_PC_PPP ~ Tasa_mortalidad)
-abline(b, col = "black", lwd = 3)
-
+a$coefficients <- abs(a$coefficients) + 100
+abline(a, col = "blue", lwd = 3 )
