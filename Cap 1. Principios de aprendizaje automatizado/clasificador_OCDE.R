77fd301f1866bffbcf8ec70f1239b56bc55b4a43
alfredo203 <alfredini31@hotmail.com>
2017-01-13 20:47:26 -0600

    OCDE todos

--- Cap 1. Principios de aprendizaje automatizado/clasificador_OCDE.R
+++ Cap 1. Principios de aprendizaje automatizado/clasificador_OCDE.R
@@ -3,13 +3,13 @@
 ## Entrenamiento
 
 ## Lectura del archivo de entrenamiento
-df <- read.csv(file = "Cap 1. Principios de aprendizaje automatizado/data_countries.csv")
+df <- read.csv(file = "Cap 1. Principios de aprendizaje automatizado/data/non_OCDE_countries.csv")
 
 ## Se observan las variables
 names(df)
 
 ## Cambiando los nombres de todas las variables
-## se utiliza una lista más sencilla de interpretar
+## se utiliza una lista de nombres más sencilla de interpretar
 
 names(df) <- c("Nombre_pais",
                "Codigo_pais",
@@ -18,21 +18,28 @@ names(df) <- c("Nombre_pais",
                "OCDE_member")
 
 ## Gráfica de dispersión de los datos seleccionados, sin distinción por clase
-par(mfrow=c(1,2))
 
-plot(df$Tasa_mortalidad, 
-     df$GNI_PC_PPP, 
+par(mfrow=c(1,2)) ## Se especifica que se quieren dos gráficas en un mismo renglón
+
+## Gráfica 1 de 2 Mortalidad infantil e ingres nacional bruto.
+plot(x = df$Tasa_mortalidad,  ## Variable X
+     y = df$GNI_PC_PPP,       ## Variable Y
      xlab = "Tasa de Mortalidad infantil por cada mil nacimientos (2015)",
      ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
 
 
 ## Estimación del modelo de regresión lineal.
 ## se especifica que el ingreso nacional bruto = f(Tasa de mortalidad)
+## la variable Y del modelo es ingreso nacional bruto
+## la variable X del modelo es Tasa de mortalidad
 
 a <- lm(data = df,  GNI_PC_PPP ~ Tasa_mortalidad)
 
-## Se pintan los casos que están mal clasificados
+## Se pintan los casos que están mal clasificados con color 3 (verde)
+
 df$OCDE_member[df$Tasa_mortalidad > 75] <- 3
+
+## Gráfica 2 de 2 Mortalidad infantil e ingres nacional bruto, colores por clasificación de país
 
 plot(df$Tasa_mortalidad, 
      df$GNI_PC_PPP, col = df$OCDE,
@@ -40,38 +47,3 @@ plot(df$Tasa_mortalidad,
      ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
 
 abline(a, col = "blue", lwd = 3)
-
-
-## Para todos los países
-library(dplyr)
-
-## Juntando bases de datos para flag OCDE
-
-A <- read.csv("Cap 1. Principios de aprendizaje automatizado/all_contries.csv", stringsAsFactors = F)
-B<- read.csv("Cap 1. Principios de aprendizaje automatizado/ocde_members.csv", stringsAsFactors = F) %>% select(-Date)
-
-df2 <- left_join(A, B)
-
-df2$OCDE_member[is.na(df2$OCDE_member)] <- 1
-
-names(df2) <-c("Nombre_pais",
-               "Codigo_pais",
-               "GNI_PC_PPP",
-               "Tasa_mortalidad",
-               "OCDE_member")
-df2$OCDE_member[df2$Nombre_pais == "World"] <- 3
-
-
-par(mfrow=c(1,1))
-plot(df2$Tasa_mortalidad, 
-     df2$GNI_PC_PPP, 
-     col = df2$OCDE_member,
-     xlab = "Tasa de Mortalidad infantil por cada mil nacimientos (2015)",
-     ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
-abline(a, col = "blue", lwd = 3)
-
-a$coefficients <- a$coefficients * -2
-abline(a, col = "blue", lwd = 3 )
-
-a$coefficients <- abs(a$coefficients) + 100
-abline(a, col = "blue", lwd = 3 )
---
+++ Cap 1. Principios de aprendizaje automatizado/clasificador_OCDE_todos.R
@@ -0,0 +1,43 @@
+## Clasificador para todos los países
+
+#install.packages("dplyr") ## librería básica de manejo de datos
+library(dplyr) ## se carga la librería
+
+## Juntando bases de datos para flag OCDE
+A <- read.csv(file = "Cap 1. Principios de aprendizaje automatizado/data/all_contries.csv", 
+              stringsAsFactors = F)
+
+B <- read.csv(file = "Cap 1. Principios de aprendizaje automatizado/data/ocde_members.csv", 
+             stringsAsFactors = F) %>% select(-Date)
+
+df2 <- left_join(A, B)
+
+## Se pintan los casos que no son miembros de la OCDE con color 1 (negro)
+df2$OCDE_member[is.na(df2$OCDE_member)] <- 1
+
+
+## Cambiando los nombres de todas las variables
+## se utiliza una lista de nombres más sencilla de interpretar
+names(df2) <-c("Nombre_pais",
+               "Codigo_pais",
+               "GNI_PC_PPP",
+               "Tasa_mortalidad",
+               "OCDE_member")
+df2$OCDE_member[df2$Nombre_pais == "World"] <- 3
+
+
+par(mfrow=c(1,1))
+plot(df2$Tasa_mortalidad, 
+     df2$GNI_PC_PPP, 
+     col = df2$OCDE_member,
+     xlab = "Tasa de Mortalidad infantil por cada mil nacimientos (2015)",
+     ylab = "Ingreso Nacional Bruto precios corrientes PPP (2015) ")
+abline(a, col = "blue", lwd = 3)
+
+## Se pueden agregar otras líneas para mejorar el poder de clasificación del algoritmo
+
+a$coefficients <- a$coefficients * -2
+abline(a, col = "blue", lwd = 3 )
+
+a$coefficients <- abs(a$coefficients) + 100
+abline(a, col = "blue", lwd = 3 )
