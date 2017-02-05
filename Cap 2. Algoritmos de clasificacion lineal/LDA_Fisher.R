36861a489b840866635c34092aacbc1310c1ba31
alfredo203 <alfredini31@hotmail.com>
2017-02-04 23:35:53 -0600

    LDA

--- Cap 2. Algoritmos de clasificacion lineal/LDA_Fisher.R
+++ Cap 2. Algoritmos de clasificacion lineal/LDA_Fisher.R
@@ -18,12 +18,28 @@ abline(v = 2.5, h = 6, lty = 3)
 abline(v = 4.8, col ="blue")
 
 
+
+## Fase de entrenamiento
 ## Selección del 70% de la información
 
 porcentaje <- 70/100
 
 num_muestras <- porcentaje * nrow(df)
+
 set.seed(4)
 test_set<- df[sample(nrow(df), num_muestras), ]
+
+
+## Ejecución del algoritmo de LDA
+
+library(MASS)
+source(file = 'Cap 2. Algoritmos de clasificacion lineal/diferencias_iris.R')
+
+modelo <- lda(data =test_set, Species ~.)
+prediccion <- predict(modelo)
+plot(modelo)
+plot(prediccion$x, col =prediccion$class)
+plot(prediccion$x, col =test_set$Species)
+diferentes <- diferencias_iris(prediccion, test_set)
 
 
