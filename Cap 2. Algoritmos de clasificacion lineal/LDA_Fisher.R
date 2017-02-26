11709cfef54bb77111e919f9e7d787ffc6a5b04c
Alfredo Olguin <alfredini31@hotmail.com>
2017-02-26 11:39:18 -0600

    Capítulo 2 LDA Fisher, código comentado

--- Cap 2. Algoritmos de clasificacion lineal/LDA_Fisher.R
+++ Cap 2. Algoritmos de clasificacion lineal/LDA_Fisher.R
@@ -1,5 +1,7 @@
 ## LDA para datos de Fisher 1936
 
+library(dplyr)
+
 ## Se carga la informacion
 df <- iris
 
@@ -8,7 +10,11 @@ head(df)
 
 ## Se grafican las 5 dimensiones de la información
 ## se colorea según la especie a la que pertenece cada flor
+<<<<<<< HEAD
+plot(df , col = df$Species)
+=======
 plot(df %>% dplyr::select(-Species), col = df$Species)
+>>>>>>> 5a5303e91b8d8973f3896557f3a8203a4db8873f
 
 ## Se grafican 2 dimensiones y se colocan líneas arbitrarias
 ## como ejemplo de segmentación manual
@@ -26,20 +32,68 @@ porcentaje <- 70/100
 
 num_muestras <- porcentaje * nrow(df)
 
-set.seed(4)
-test_set<- df[sample(nrow(df), num_muestras), ]
+## Muestreo aleatorio
+set.seed(20)
+train_ind <- sample(seq_len(nrow(df)), size = num_muestras)
 
+## Seleccionamos las muestras que pertenecen
+## al set de entrenamiento
+train_set <- df[train_ind, ]
+
+## Seleccionamos el resto de muestras
+## que pertenecen al set de pruebas
+test_set <- df[-train_ind, ]
 
 ## Ejecución del algoritmo de LDA
 
 library(MASS)
 source(file = 'Cap 2. Algoritmos de clasificacion lineal/diferencias_iris.R')
 
-modelo <- lda(data =test_set, Species ~.)
+modelo <- lda(data =train_set, Species ~.)
+
+## Proabilidades a priori del modelo
+modelo$prior
+
+## Número de observaciones por clase
+modelo$counts
+
+## Se predice el modelo sobre el set
+## de entrenamiento, por default 
+## no se especifica un dataset nuevo
 prediccion <- predict(modelo)
-plot(modelo)
-plot(prediccion$x, col =prediccion$class)
-plot(prediccion$x, col =test_set$Species)
+
+
+
+## Gráfica de clasificación original
+par(mfrow=c(2,1))
+plot(prediccion$x, 
+     col =train_set$Species,
+     main = "Original")
+
+## Gráfica de clasificación según el modelo estimado
+plot(prediccion$x, 
+     col =prediccion$class,
+     main = "Predicción")
+
+## Matriz de confusión
+## Se muestran las clasificaciones correctas
+## en la diagonal para el set de entrenamiento
+table(prediccion$class, train_set$Species)
+
+
+## Gráfica de barras por número de observaciones
+barplot(summary(train_set$Species), main = "Conjunto de pruebas")
+
+barplot(summary(test_set$Species), main = "Conjunto de entrenamiento")
+
+## Validación del modelo LDA
+## Se ejecutan las predicciones sobre
+## el set de pruebas con el modelo original "modelo"
+prediccion <- predict(modelo, newdata = test_set)
+
+## Matriz de confusión
+## Se muestran las clasificaciones correctas
+## en la diagonal para el set de pruebas
+table(prediccion$class, test_set$Species)
+
 diferentes <- diferencias_iris(prediccion, test_set)
-
-
